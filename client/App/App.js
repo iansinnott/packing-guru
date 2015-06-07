d34ff48258f6f92c3bc3d748f2a22189e4ec3b4c
Ian Sinnott <ian@iansinnott.com>
2015-06-07 12:22:27 -0700

    Initial actual commit for packing guru

--- client/App/App.js
+++ client/App/App.js
@@ -5,14 +5,31 @@ require('./App.styl');
 require('font-awesome-webpack');
 
 let App = React.createClass({
+
+  getInitialState() {
+    return {
+      locationText: ''
+    };
+  },
+
   render() {
     return (
       <div className="App container">
-        <h1>React Boilerplate</h1>
-        <p>What you know about React?</p>
+        <h1>Travel Guru</h1>
+        <input
+          type="text"
+          value={this.state.locationText}
+          onChange={this.handleTextEntry}
+          placeholder='Where are you going?' />
       </div>
     );
+  },
+
+  handleTextEntry(e) {
+    var locationText = e.target.value;
+    this.setState({ locationText });
   }
+
 });
 
 module.exports = App;
